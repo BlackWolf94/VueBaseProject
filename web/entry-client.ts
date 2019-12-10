983b0f7f98767902ad41ad836aedcafbbd1dacc8
Dmytro Zataidukh <zidadindimon@gmail.com>
2019-12-10 10:50:03 +0200

    srr context remove initial state from script after mount

--- web/entry-client.ts
+++ web/entry-client.ts
@@ -1,23 +1,30 @@
 import 'es6-promise/auto';
-import {createApp} from '@web/createApp';
+import { createApp } from '@web/createApp';
 
 const state = (window as any).__INITIAL_STATE__ || {};
 const appConf = (window as any).__INITIAL_APP_CONF__ || {};
 
 
-const {app, router} = createApp(appConf, state);
+const { app, router } = createApp(appConf, state);
 
 (window as any).runtime_process_env = {
-    DEBUG: true,
+  DEBUG: true
+};
+
+
+const onReady = () => {
+  Array.from(document.getElementsByTagName('script'))
+    .forEach((el) =>
+      el.innerHTML = el.innerHTML.match('__INITIAL_') ? '' : el.innerHTML);
+
+  // Add router hook for handling asyncData.
+  // Doing it after initial route is resolved so that we don't double-fetch
+  // the data that we already have. Using router.beforeResolve() so that all
+
+  // actually mount to DOM
+  app.$mount('#app');
 };
 
 // wait until router has resolved all async before hooks
 // and async components...
-router.onReady(() => {
-    // Add router hook for handling asyncData.
-    // Doing it after initial route is resolved so that we don't double-fetch
-    // the data that we already have. Using router.beforeResolve() so that all
-
-    // actually mount to DOM
-    app.$mount('#app');
-});
+router.onReady(onReady);
